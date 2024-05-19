4e33115aadd9dd7dd53f93413d6ac637709f1ba1
HamzaRaza786 <hamza.raza2015@outlook.com>
2024-05-19 13:02:01 +0500

    Applied the fix for first character not showing up. The issue was that we were skipping 0th index of column hence why it was skipping a character. Moreover, in this commit we also have code for unit testing blog. As I was in between of it.

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,26 @@
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 use core::panic::PanicInfo;
+
+use vga_buffer::print_something;
+mod serial;
 mod vga_buffer;
 static HELLO: &[u8] = b"Hello World!";
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failure = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
@@ -20,14 +38,18 @@ pub extern "C" fn _start() -> ! {
     // }
     //
     use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("Hello again\n").unwrap();
+    vga_buffer::WRITER
+        .lock()
+        .write_str("Hello again\n")
+        .unwrap();
     write!(
         vga_buffer::WRITER.lock(),
-        ", some numbers: {} {}",
+        "some numbers: {} {}",
         42,
         1.337
     )
     .unwrap();
+    // print_something();
 
     #[cfg(test)]
     test_main();
@@ -42,8 +64,16 @@ fn panic(info: &PanicInfo) -> ! {
 
 #[cfg(test)]
 pub fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
+    serial_println!("Running {} tests", tests.len());
     for test in tests {
         test();
     }
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    serial_print!("trivial assertion... ");
+    assert_eq!(0, 1);
+    serial_println!("[ok]");
 }
