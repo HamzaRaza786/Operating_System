689420ff0185838735cb1c023854af417ab53cfd
HamzaRaza786 <hamza.raza2015@outlook.com>
2024-08-07 23:54:51 +0500

    Updated main

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ static HELLO: &[u8] = b"Hello World!";
 #[repr(u32)]
 pub enum QemuExitCode {
     Success = 0x10,
-    Failure = 0x11,
+    Failed = 0x11,
 }
 
 pub fn exit_qemu(exit_code: QemuExitCode) {
@@ -42,38 +42,62 @@ pub extern "C" fn _start() -> ! {
         .lock()
         .write_str("Hello again\n")
         .unwrap();
-    write!(
-        vga_buffer::WRITER.lock(),
-        "some numbers: {} {}",
-        42,
-        1.337
-    )
-    .unwrap();
+    write!(vga_buffer::WRITER.lock(), "some numbers: {} {}", 42, 1.337).unwrap();
     // print_something();
 
+    use x86_64::registers::control::Cr3;
+
+    let (level_4_page_table, _) = Cr3::read();
+    println!(
+        "Level 4 page table at: {:?}",
+        level_4_page_table.start_address()
+    );
     #[cfg(test)]
     test_main();
     loop {}
 }
 
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
 }
 
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
 #[cfg(test)]
 pub fn test_runner(tests: &[&dyn Fn()]) {
     serial_println!("Running {} tests", tests.len());
     for test in tests {
-        test();
+        test.run();
     }
     exit_qemu(QemuExitCode::Success);
 }
 
 #[test_case]
 fn trivial_assertion() {
-    serial_print!("trivial assertion... ");
-    assert_eq!(0, 1);
+    // serial_print!("trivial assertion... ");
+    assert_eq!(1, 1);
     serial_println!("[ok]");
 }
